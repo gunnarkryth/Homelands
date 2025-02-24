ce1e8c87caf32295d7fd1514cfc52935934250da
Gunski <gunnarkryth@gmail.com>
2025-02-24 12:08:26 +0100

    #8

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,51 @@
-import { Container } from "@mui/material";
+import { Container, Toolbar, MenuItem, Typography } from "@mui/material";
+import { TwitterIcon, FacebookIcon } from "@mui/icons-material";
+import { Logo } from "./Logo";
 
 export const Footer = () => {
   return (
     <Container disableGutters>
-      <footer>AAAAAAAAAAAAAAAAA</footer>
+      <footer>
+        <Logo />
+        <Toolbar>
+          <MenuItem>
+            <ul>
+              <li>
+                <Typography variant="body1" color="initial">
+                  Øster Uttrupvej 5
+                </Typography>
+              </li>
+              <li>
+                <Typography variant="body1" color="initial">
+                  9000 Aalborg
+                </Typography>
+              </li>
+            </ul>
+          </MenuItem>
+          <MenuItem>
+            <ul>
+              <li>
+                <Typography variant="body1" color="initial">
+                  Email: info@homelands.dk
+                </Typography>
+              </li>
+              <li>
+                <Typography variant="body1" color="initial">
+                  Telefon: +45 11 22 33 44
+                </Typography>
+              </li>
+            </ul>
+          </MenuItem>
+        </Toolbar>
+        <ul>
+          <li>
+            <svg data-testid="TwitterIcon" />
+          </li>
+          <li>
+            <svg data-testid="FacebookIcon" />
+          </li>
+        </ul>
+      </footer>
     </Container>
   );
 };
