b70b5479c3adba3dd2696bc77c72a9d456555784
Gunski <gunnarkryth@gmail.com>
2025-02-24 10:50:48 +0100

    #45

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainLayout } from "./layout/MainLayout";
 import { Home } from "./pages/Home";
 import { Houses } from "./pages/Houses";
+import { HouseDetails } from "./pages/HouseDetails";
 import { Login } from "./pages/Login";
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           //Replace "a" with element
           <Route index element={<Home />} />
-          <Route path="houses" element={<Houses />} />
+          <Route path="boliger" element={<Houses />} />
+          <Route path="bolig-detaljer" element={<HouseDetails />} />
           <Route path="login" element={<Login />} />
         </Route>
       </Routes>
