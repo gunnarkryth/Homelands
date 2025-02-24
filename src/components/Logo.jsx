dad8cb0dc7a7792cabb89b17cabf376fd8ab5f87
Gunski <gunnarkryth@gmail.com>
2025-02-24 10:11:04 +0100

    #7

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material";
 
 export const Logo = () => {
   return (
     <Box>
-      <Typography variant="h1" color="initial">Homelands</Typography>
+      <Typography variant="h1" color="initial">
+        Homelands
+      </Typography>
     </Box>
-  )
-}
+  );
+};
