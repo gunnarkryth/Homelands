c9e6c3362edb02c9f15fd2ce2dea13761805ab75
Gunski <gunnarkryth@gmail.com>
2025-02-24 10:53:58 +0100

    #42

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Routes
 import { MainLayout } from "./layout/MainLayout";
 import { Home } from "./pages/Home";
 import { Houses } from "./pages/Houses";
 import { HouseDetails } from "./pages/HouseDetails";
 import { Login } from "./pages/Login";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          //Replace "a" with element
           <Route index element={<Home />} />
           <Route path="boliger" element={<Houses />} />
           <Route path="bolig-detaljer" element={<HouseDetails />} />
           <Route path="login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import Typography from "@mui/material/Typography";
+export const NotFound = () => {
+  return (
+    <>
+      <Typography variant="h2" color="initial"></Typography>
+      404
+    </>
+  );
+};
