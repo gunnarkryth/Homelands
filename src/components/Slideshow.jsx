11bc24e204f02c5f0f8c7e118a93d3c3f2edee88
Gunski <gunnarkryth@gmail.com>
2025-02-26 08:00:37 +0100

    #10

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,13 +1,21 @@
-import { Box, IconButton } from "@mui/material";
+import { Box, CircularProgress, IconButton } from "@mui/material";
 import useFetch from "../hooks/useFetch";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { useEffect, useState } from "react";
 
 export const Slideshow = () => {
   const { data, loading, error, request } = useFetch();
 
-  const handleGet = () => {
-    request("https://api.mediehuset.net/homelands/images");
-  };
+  //Liste af billeder
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const handleGet = () => {
+      request("https://api.mediehuset.net/homelands/images");
+    };
+  }, [request]);
+
+  if (loading) return <CircularProgress color="accent.primary" />;
 
   return (
     <Box>
