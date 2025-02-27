7b261ed0a987e56e71acc980bea603673cb5c3b1
Gunski <gunnarkryth@gmail.com>
2025-02-27 08:59:23 +0100

    #9

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,28 @@
-import { Box, TextField, IconButton } from "@mui/material";
+import { Box, TextField, IconButton, Button } from "@mui/material";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export const SearchBar = () => {
   const [searchWord, setSearchWord] = useState();
 
+  console.log("searchWord", searchWord);
+
   return (
-    <Box>
+    <Box display={"flex"} alignContent={"center"} justifyContent={"center"}>
       <TextField
-        variant="outlined"
-        label="Søg"
+        type="search"
+        variant="standard"
+        label="Search"
+        name="search"
+        id="search-field"
         value={searchWord}
         onChange={(event) => setSearchWord(event.target.value)}
       ></TextField>
-      <IconButton>
-        <NavLink to={`/search/${searchWord}`}></NavLink>
-      </IconButton>
+      <NavLink to={searchWord ? `/search/${searchWord}` : `/housing`}>
+        <Button variant="outlined" title="Søg" color="white">
+          Søg
+        </Button>
+      </NavLink>
     </Box>
   );
 };
