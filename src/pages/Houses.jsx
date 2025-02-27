bb838585713dfa53bd94dc3d1109db7a24c54590
Gunski <gunnarkryth@gmail.com>
2025-02-27 08:31:01 +0100

    #

--- src/pages/Houses.jsx
+++ src/pages/Houses.jsx
@@ -1,9 +1,48 @@
-import { useParams } from "react-router-dom";
+import { Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { useEffect, useState } from "react";
+import { HousingCard } from "../components/HousingCard";
+import { useParams } from "react-router";
+import { findValuesInObject } from "../helpers/findValuesInObject";
 
-export const Houses = () => {
-  const { keyWord } = useParams();
+export const HousingPage = () => {
+  const [houseData, setHouseData] = useState();
 
-  console.log(keyWord);
+  const { keyword } = useParams();
 
-  return <></>;
+  useEffect(() => {
+    console.log("key", keyword);
+    fetch("https://api.mediehuset.net/homelands/homes")
+      .then((res) => res.json())
+      .then((data) =>
+        keyword !== null &&
+        keyword !== "" &&
+        keyword !== "undefined" &&
+        keyword !== undefined
+          ? setHouseData({
+              items: data.items.filter((item) =>
+                findValuesInObject(item, keyword)
+              ),
+            })
+          : setHouseData(data)
+      )
+      .catch((err) => console.log("error: ", err));
+  }, [keyword]);
+
+  return (
+    <Container>
+      <Typography textAlign="center" margin="2vh" variant="h3">
+        Housing Page
+      </Typography>
+      <Grid container rowSpacing="1" columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        {houseData?.items?.map((item) => {
+          return (
+            <Grid size={4} key={item.id}>
+              <HousingCard item={item} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
 };
